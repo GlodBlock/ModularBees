a9a4c46ca6560670439958d3470a766edaeaf4ca
GlodBlock <1356392126@qq.com>
2025-10-05 21:50:14 +0800

    fix https://github.com/GlodBlock/ModularBees/issues/1

--- src/main/java/com/glodblock/github/modularbees/common/tileentities/hive/TileBeehiveOverclocker.java
+++ src/main/java/com/glodblock/github/modularbees/common/tileentities/hive/TileBeehiveOverclocker.java
@@ -43,11 +43,12 @@ public class TileBeehiveOverclocker extends TileBeehivePart implements ItemHandl
                     this.running = this.findRecipe(stack);
                 }
                 if (this.running != null) {
+                    var boost = this.running.power();
                     if (this.energy.getEnergyStored() >= bees * POWER_USE) {
                         int power = this.energy.forceExtractEnergy(bees * POWER_USE, false);
                         if (power >= bees * POWER_USE) {
                             stack.hurtAndBreak(1, server, null, item -> this.electrode.setStackInSlot(0, ItemStack.EMPTY));
-                            return this.running.power();
+                            return boost;
                         }
                     }
                 }
